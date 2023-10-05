25feab58db5adb0099a12acb95dd512808b21fa6
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-10-05 07:15:10 +0600

    update: academiv semester addd

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,9 @@
 import express from 'express';
+import { academicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 
 const router = express.Router();
 
-const moduleRoutes = [
-  {
-    path: '',
-    routes: ""
-  }
-];
+const moduleRoutes = [{ path: '/academic-semesters', routes: academicSemesterRoutes }];
 
 moduleRoutes.forEach((route) => {
   router.use(route.path, route.routes);
